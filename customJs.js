b23e3c93aa42c8a251c9d826e7b3b4edbf9144cd
Mahfuzul Haque <mafhaque@darenasolutions.com>
2018-11-17 01:54:27 -0500

    -started work on risk table tool -set up bootstrap config -added navbar -laid out the UI for risk table form and risk table

---
+++ customJs.js
@@ -0,0 +1,6 @@
+let risk = document.querySelector('#risk');
+let riskCategory = document.querySelector('#risk-category');
+let riskProbability = document.querySelector('#risk-probability');
+let riskImpact = document.querySelector('#risk-impact');
+let riskForm = document.querySelector('.risk-form');
+let riskTable = document.querySelector('.risk-table');
