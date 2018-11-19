d3216c1cf05dcc186f2d5e79bca4b53cfe2ae43e
Mahfuzul Haque <mafhaque@darenasolutions.com>
2018-11-18 19:26:41 -0500

    -Implemented ability to add a risk to the risk table

--- customJs.js
+++ customJs.js
@@ -4,3 +4,26 @@ let riskProbability = document.querySelector('#risk-probability');
 let riskImpact = document.querySelector('#risk-impact');
 let riskForm = document.querySelector('.risk-form');
 let riskTable = document.querySelector('.risk-table');
+let riskTableBody = riskTable.querySelector('tbody');
+
+riskForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let riskRow = riskTableBody.insertRow(0);
+	let riskCell = riskRow.insertCell(0);
+	riskCell.style = 'max-width: 200px';
+	let riskCategoryCell = riskRow.insertCell(1);
+	let riskProbabilityCell = riskRow.insertCell(2);
+	let riskImpactCell = riskRow.insertCell(3);
+	let riskActions = riskRow.insertCell(4);
+	
+	riskCell.innerText = risk.value;
+	riskCategoryCell.innerText = riskCategory.value;
+	riskProbabilityCell.innerText = `${riskProbability.value}%`;
+	riskImpactCell.innerText = riskImpact.value;
+	riskActions.insertAdjacentHTML('afterbegin',  
+		'<div class="btn-group">' +
+			'<button type="button" class="btn btn-danger"><i class="fas fa-trash"></i></button><button type="button" class="btn btn-primary"><i class="fas fa-pen"></i></button>' + 
+		'</div>'
+ 	);
+	riskForm.reset();
+})
